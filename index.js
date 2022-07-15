cd1dcaeb49bf49f0a25a91a713d2c9232ada17d9
Tyler <toygrrr@gmail.com>
2022-07-15 10:54:50 -0700

    java stuff

---
+++ index.js
@@ -0,0 +1,10 @@
+function diffImage(img){
+   if(img.src.match(/blank/)) img.src = "img/spoongebob.gif";
+   else img.src = "img/spoongebob.gif";
+}
+
+function diffImageTwo(img){
+    if(img.src.match(/blank/)) img.src = "img/renstimpy.gif";
+    else img.src = "img/renstimpy.gif";
+ }
+ 

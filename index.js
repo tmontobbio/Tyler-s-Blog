fd86bdb2147c30b6274fcfacb7f890c984af8145
Tyler <toygrrr@gmail.com>
2022-08-05 14:11:05 -0700

    fetch/post updated

--- index.js
+++ index.js
@@ -1,55 +1,52 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let addPost = false;
+  let postDrawer = false;
   const newPostBtn = document.querySelector("#new-post-btn");
   const formContainer = document.querySelector("#form-container");
   const postForm = document.querySelector("#new-post-form");
   const blogContainer = document.querySelector("#blog-container");
   fetchPosts();
 
+  function generatePost(data) {
+    const postContainer = document.createElement("div");
+    const title = document.createElement("h1");
+    title.textContent = data.title;
+    const body = document.createElement("p");
+    body.textContent = data.body;
+    postContainer.className = "blog-post";
+    title.className = "blog-title";
+    body.className = "blog-body";
+    blogContainer.append(postContainer);
+    postContainer.append(title);
+    postContainer.append(body);
+  }
+
   function fetchPosts() {
     fetch(" http://localhost:3000/posts")
       .then((res) => res.json())
-      .then((data) =>
-        data.forEach((post) => {
-          const postContainer = document.createElement("div");
-          const title = document.createElement("h1");
-          title.textContent = post.title;
-          const body = document.createElement("p");
-          body.textContent = post.body;
-          postContainer.className = "blog-post";
-          title.className = "blog-title";
-          body.className = "blog-body";
-          blogContainer.append(postContainer);
-          postContainer.append(title);
-          postContainer.append(body);
-        })
-      );
+      .then((data) => data.forEach((data) => generatePost(data)));
   }
+
   newPostBtn.addEventListener("click", () => {
-    addPost = !addPost;
-    if (addPost) {
+    postDrawer = !postDrawer;
+    if (postDrawer) {
       formContainer.style.display = "block";
     } else {
       formContainer.style.display = "none";
     }
   });
 
-  // Post listener and JSON post
   postForm.addEventListener("submit", (e) => {
-    e.preventDefault();
-    e.target.reset();
-
-    addPost = !addPost;
-    if (addPost) {
+    postDrawer = !postDrawer;
+    if (postDrawer) {
       formContainer.style.display = "block";
     } else {
       formContainer.style.display = "none";
     }
-    const titleText = document.querySelector(".input-text");
-    const formBody = document.querySelector("#text-box");
+    const textTitle = document.querySelector("#input-text");
+    const textBody = document.querySelector("#text-box");
     let newPost = {
-      title: `${titleText.textContent}`, // cant get this to work
-      body: `${formBody.textContent}`,
+      title: textTitle.value, // cant get this to work
+      body: textBody.value,
     };
     fetch(`http://localhost:3000/posts`, {
       method: "POST",
@@ -57,7 +54,10 @@ document.addEventListener("DOMContentLoaded", () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(newPost),
-    }).then((res) => res.json);
+    })
+      .then((res) => res.json)
+      .then((data) => generatePost(data));
+    e.target.reset();
   });
 });
 
@@ -70,15 +70,3 @@ function diffImageTwo(img) {
   if (img.src.match(/blank/)) img.src = "img/renstimpy.gif";
   else img.src = "img/renstimpy.gif";
 }
-
-// function renderPost() {
-//   const titleText = document.querySelector(".input-text");
-//   const formBody = document.querySelector("#text-box");
-//   if (titleText.value === "") {
-//     alert("Please fill out title & body.");
-//   } else if (formBody.value === "") {
-//     alert("Please fill out title & body.");
-//   } else {
-//
-//   }
-// }
